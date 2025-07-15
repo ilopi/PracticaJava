dd682d764d1bc3c3488ef87642cc01124d89f002
Isabel SL <isabel.sanchez@nter.es>
2025-07-15 12:59:50 +0200

    Genéricos

---
+++ src/JavaModerno_JavaPDF10/Generics.java
@@ -0,0 +1,23 @@
+package JavaModerno_JavaPDF10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generics {
+
+    public static void main(String[] args) {
+
+        /*
+         * Los Generics (genéricos) permiten crear
+         * clases, métodos y estructuras de datos que pueden trabajar
+         * con cualquier tipo de objeto.
+         * List<Tipo> lista = new ArrayList<>();
+         *
+         *
+         */
+
+        List<String> nombres = new ArrayList<>();
+        List<Integer> edades = new ArrayList<>();
+
+    }
+}
